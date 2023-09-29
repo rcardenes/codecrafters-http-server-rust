9cd7e43338271050161e43a3b81abf8d3592d1b2
Ricardo Cárdenes <ricardo.cardenes@noirlab.edu>
2023-09-28 23:42:55 -0300

    Refactor the server into a Tokio async app

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,37 @@
-use std::io::{BufRead, BufReader, Write};
-use std::net::TcpListener;
+use tokio::io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::net::{TcpListener, TcpStream};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+async fn handle_connection(stream: TcpStream) -> io::Result<()> {
+    let mut reader = BufReader::new(stream);
+    let mut buf = String::new();
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                let mut reader = BufReader::new(stream.try_clone().unwrap());
-                let mut buf = String::new();
-                reader.read_line(&mut buf).unwrap();
-                stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+    // Read header
+    reader.read_line(&mut buf).await?;
+    reader.write(b"HTTP/1.1 200 OK\r\n\r\n").await?;
+
+    Ok(())
+}
+
+const SERVER_ADDRESS: &str = "127.0.0.1:4221";
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind(SERVER_ADDRESS).await?;
+
+    loop {
+        match listener.accept().await {
+            Ok((stream, addr)) => {
+                eprintln!("Accepted connection from: {addr}");
+                tokio::spawn(async move {
+                    handle_connection(stream).await
+                        .map_err(|error| {
+                            eprintln!("Handling connection: {error}");
+                            Ok::<_, io::Error>(())
+                        }).unwrap();
+                });
             }
-            Err(e) => {
-                println!("error: {}", e);
+            Err(error) => {
+                eprintln!("Accepting incoming connection: {error}");
             }
         }
     }
